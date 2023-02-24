3666df8143d2fb6e270f36107e4a3e8c1e9573a8
정민아 <octavesop@gmail.com>
2023-02-25 00:28:44 +0900

    :tada: initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import logoImage from "./images/logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <img width="85%" src={logoImage} alt="logo"></img>
+        </div>
+        <div class="text-component">
+          Lorem ipsum MAIN dolor sit amet, consectetur adipiscing elit, sed
+          INTRODUCE do eiusmod tempor incididun t ut labore et dolore PORTFOLIO
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco consequat. Duis aute irure BLOG dolor in reprehenderit in
+          voluptate velit esse cillum dolore eu
+        </div>
+      </header>
+      <div class="App-body">
+        <div>후하하</div>
+      </div>
+      <footer>정민아가 햇대용</footer>
+    </div>
+  );
+}
+
+export default App;
